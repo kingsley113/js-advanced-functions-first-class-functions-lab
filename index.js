58a41a07652695d0f4e7e1b1b26009a9f83f36e8
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-09-15 02:21:33 -0700

    commit tree 4bea5c199d52897db0e75a7649db98a6d859d678

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(array) {
+  return array.slice(0, 2);
+}
+
+function returnLastTwoDrivers(array) {
+  return array.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+  return function (fare) {
+    return fare * multiplier;
+  };
+}
+
+const fareDoubler = function (fare) {
+  return createFareMultiplier(2)(fare);
+};
+
+const fareTripler = function (fare) {
+  return createFareMultiplier(3)(fare);
+};
+
+function selectDifferentDrivers(array, selector) {
+  return selector(array);
+}
